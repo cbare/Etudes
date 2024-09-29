e99eb97792e1dcc0a46d351e4c7e78bfa69b7c61
Christopher Bare <christopher.bare@volparahealth.com>
2024-09-30 11:23:07 +1300

    walk_json with yield from

--- python/walk_json.py
+++ python/walk_json.py
@@ -1,7 +1,6 @@
 """
 Walk a JSON-like data structure and yield the path to each leaf and its value.
 """
-
 data = {
     "a": {
         "b": {
@@ -14,21 +13,16 @@ data = {
 }
 
 
-
-def walk_json(data):
-    prefix = []
-    stack = [(data, prefix)]
-
-    while stack:
-        data, prefix = stack.pop()
-        if isinstance(data, dict):
-            for key, value in data.items():
-                stack.append((value, prefix + [key]))
-        elif isinstance(data, list):
-            for i, value in enumerate(data):
-                stack.append((value, prefix + [str(i)]))
-        else:
-            yield ('.'.join(prefix), data)
+def walk_json(data, prefix=None):
+    prefix = prefix or []
+    if isinstance(data, dict):
+        for key, value in data.items():
+            yield from walk_json(value, prefix + [key])
+    elif isinstance(data, list):
+        for i, value in enumerate(data):
+            yield from walk_json(value, prefix + [str(i)])
+    else:
+        yield ('.'.join(prefix), data)
 
 for k,v in walk_json(data):
     print(k, v)
